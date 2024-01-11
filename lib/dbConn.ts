df93b4cee0adb4bc43964f01c0c7817e0663312e
Sabmus <abiatar.ms@gmail.com>
2024-01-11 18:53:46 -0300

    refactor: added listener to db connection and prevent reconnect when already connected

--- lib/dbConn.ts
+++ lib/dbConn.ts
@@ -1,8 +1,29 @@
-import mongoose from 'mongoose';
+import mongoose, { ConnectionStates } from 'mongoose';
+
+interface Connection {
+  isConnected: ConnectionStates | null;
+}
+
+const connection: Connection = {
+  isConnected: null,
+};
 
 async function dbConnect() {
+  mongoose.connection.on('connected', () => console.log('connected'));
+  mongoose.connection.on('open', () => console.log('open'));
+  mongoose.connection.on('disconnected', () => console.log('disconnected'));
+  mongoose.connection.on('reconnected', () => console.log('reconnected'));
+  mongoose.connection.on('disconnecting', () => console.log('disconnecting'));
+  mongoose.connection.on('close', () => console.log('close'));
+
   try {
-    await mongoose.connect(process.env.MONGODB_URI!);
+    if (connection.isConnected) {
+      console.log('already connected');
+
+      return;
+    }
+    const db = await mongoose.connect(process.env.MONGODB_URI!);
+    connection.isConnected = db.connection.readyState;
   } catch (error) {
     throw new Error('Error connecting to db.');
   }
